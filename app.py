cf3f61758e16f0c7023633d0d57708341310d461
mariab432 <maria.alvisb@campusucc.edu.co>
2024-11-19 15:19:53 -0500

    nuevas rutas

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from flask import Flask, render_template #type: ignore
 app = Flask(__name__)
 
 @app.route("/")
+def login():
+    return render_template('login.html')
+
+@app.route("/registro")
+def registro():
+    return render_template('registro.html')
+
+@app.route("/index")
 def index():
     return render_template('index.html')
 
