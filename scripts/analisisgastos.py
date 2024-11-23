6bb0c8e79f1e07bd994d61fc432f3c42200e4a4b
Pathinker <danielguzmanzavala004@gmail.com>
2024-11-22 22:19:05 -0600

    Analisis Gastos: Primeras gráficas

--- scripts/analisisgastos.py
+++ scripts/analisisgastos.py
@@ -2,45 +2,83 @@ from flask import Flask, render_template, send_file, redirect,url_for, session, 
 from flask import send_file
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import io
-import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
 def obtenerInformacion(conexion):
-
-    stringBusqueda = "Dinero reservado actualizado"
-
+    
     cursor = conexion.cursor()
     cursor.execute('SELECT ingresos, fecha FROM movimientos WHERE id_usuario = %s;', (session.get('user_id'),))
     ingresos = cursor.fetchall()
-    cursor.execute('SELECT egresos, fecha FROM movimientos WHERE id_usuario = %s AND descripcion_movimiento != %s;', (session.get('user_id'),stringBusqueda,))
+    cursor.execute('SELECT egresos, descripcion_movimiento, fecha FROM movimientos WHERE id_usuario = %s;', (session.get('user_id'),))
     egresos = cursor.fetchall()
-    cursor.execute('SELECT egresos, fecha FROM movimientos WHERE id_usuario = %s AND descripcion_movimiento = %s;', (session.get('user_id'),stringBusqueda,))
-    dineroReservado = cursor.fetchall()
     cursor.close()
 
-    return ingresos, dineroReservado, egresos
+    return ingresos, egresos
 
-def agruparFecha(tupla):
+def agruparIngresos(tupla):
 
     auxiliar = []
     cantidadMes = 0
     mesAnterior = None
+    cambio = None
 
     for monto, fecha in tupla:
 
+        cambio = True
         cantidadMes += monto
 
-        if(mesAnterior is None or fecha.month != mesAnterior):
+        if(mesAnterior is None and fecha.month != mesAnterior):
 
             auxiliar.append(cantidadMes)
             cantidadMes = 0
 
         mesAnterior = fecha.month
+    
+    if(cambio is True): # Casos especiales para tratar un solo elemento o el último elemento en un mes no cambiante
+
+        auxiliar.append(cantidadMes)
 
     return auxiliar
 
+def agruparEgresos(tupla):
+
+    dineroReservado = []
+    egresos = []
+    cadenaBusqueda = "Dinero reservado actualizado"
+
+    reservadoMes = 0
+    egresosMes = 0
+
+    mesAnterior = None
+    cambio = None
+
+    for monto, movimiento, fecha in tupla:
+
+        cambio = True
+
+        if(movimiento == cadenaBusqueda):
+            reservadoMes += monto
+        else:
+            egresosMes += monto
+
+        if(mesAnterior is not None and fecha.month != mesAnterior): # Actualizar al detectar nuevo mes
+
+            egresos.append(egresosMes)
+            dineroReservado.append(reservadoMes)
+
+            reservadoMes = 0
+            egresosMes = 0
+
+        mesAnterior = fecha.month
+
+    if(cambio is True): # Casos especiales para tratar un solo elemento o el último elemento en un mes no cambiante
+
+        egresos.append(egresosMes)
+        dineroReservado.append(reservadoMes)
+
+    return dineroReservado, egresos
+        
 def obtenerMeses(tupla):
 
     meses = {1 : "Enero", 2 : "Febrero", 3 : "Marzo", 4 : "Abril", 5 : "Mayo", 6 : "Junio", 7 : "Julio", 8 : "Agosto", 9 : "Septiembre", 10 : "Octubre", 11 : "Noviembre", 12 : "Diciembre"}
@@ -59,28 +97,41 @@ def obtenerMeses(tupla):
 
 def graficar(conexion):
 
-    ingresos, dineroReservado, egresos = obtenerInformacion(conexion)
+    ingresos, egresos = obtenerInformacion(conexion)
     meses = obtenerMeses(ingresos)
 
-    ingresosMeses = agruparFecha(ingresos)
-    reservadoMeses = agruparFecha(dineroReservado)
-    egresosMeses = agruparFecha(egresos)
+    ingresosMeses = agruparIngresos(ingresos)
+    reservadoMeses, egresosMeses =  agruparEgresos(egresos)
 
     print("Ingresos: ", ingresosMeses)
     print("Reservado: ", reservadoMeses)
     print("Egresos:", egresosMeses)
     print("Meses:", meses)
         
-    fig, ax = plt.subplots(figsize=(6, 6))
-    sns.set_style("darkgrid") 
+    fig, ax = plt.subplots(figsize=(4, 7))
+    sns.set_style("whitegrid") 
 
-    x = [i for i in range(1000)]
-    y = [i for i in range(1000)]
+    x = range(len(meses))
 
-    sns.lineplot(x=x, y=y)
-    canvas = FigureCanvas(fig)
+    ax.bar(x, ingresosMeses, label="Ingresos", color="green")
+    ax.bar(x, reservadoMeses, bottom=ingresosMeses, label="Reservado", color="orange")
+    ax.bar(x, egresosMeses, bottom=[i + r for i, r in zip(ingresosMeses, reservadoMeses)], 
+           label="Egresos", color="red")
+
+    ax.set_xticks(x)
+    ax.set_xticklabels(meses, rotation=45)
+    ax.set_title("Gráfica de Gastos en Función de Ingresos")
+    ax.legend()
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+
+    canvas = plt.gcf()
     img = io.BytesIO()
-    fig.savefig(img, format="png")
+    canvas.savefig(img, format="png", bbox_inches="tight", transparent=True)
     img.seek(0)
 
     return send_file(img, mimetype="image/png")
